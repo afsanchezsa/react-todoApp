d6175a8daf2c2256cc77aebcfc636fcd710f3236
unknown <afsanchezsa@unal.edu.co>
2022-02-18 12:36:59 -0500

    maquetacion de componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {TodoCounter} from './components/TodoCounter';
+import {TodoSearch} from './components/TodoSearch';
+import {TodoList} from './components/TodoList';
+import {TodoItem} from './components/TodoItem';
+import {CreateTodoButton} from './components/CreateTodoButton';
+const tasks=[
+{'id':1,'text':'tarea 1', 'completed':false},
+{'id':2,'text':'tarea 2', 'completed':false},
+{'id':3,'text':'tarea 3', 'completed':false}
+
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+  <TodoCounter/>
+  
+  <TodoSearch/>
+  
+  <TodoList>
+    {tasks.map(t=>(<TodoItem key={t.id} text={t.text}/>))}
+    
+  </TodoList>
+  <CreateTodoButton/>
+
+  </React.Fragment>
   );
 }
 
