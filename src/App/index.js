b04cad6f0a280ac243a2438922910c1363781faa
unknown <afsanchezsa@unal.edu.co>
2022-02-21 10:50:24 -0500

    uso del local storage

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { AppUI } from './AppUI';
 
-const defaulTasks = [
+/*const defaulTasks = [
   { 'id': 1, 'text': 'tarea 1', 'completed': true },
   { 'id': 2, 'text': 'tarea 2', 'completed': false },
   { 'id': 3, 'text': 'tarea 3', 'completed': false }
 
-]
+]*/
 
 
 function App() {
+  const localStorageTodos=localStorage.getItem('TODOS_V1');
+  let defaulTasks;
+  if(!localStorageTodos){
+    localStorage.setItem('TODOS_V1',JSON.stringify([]));
+    defaulTasks=[]
+  }else{
+    defaulTasks=JSON.parse(localStorage.getItem('TODOS_V1'));
+  }
   const [tasks, setTasks] = React.useState(defaulTasks);
   const [searchValue, setSearchValue] = React.useState('');
   const completedTodos = tasks.filter(t => t.completed).length;
@@ -18,21 +26,24 @@ function App() {
   if (!searchValue.length >= 1) {
     searchedTodos = tasks;
   }else{
-    searchedTodos=tasks.filter(t=>t.text.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()) )
+    searchedTodos=tasks.filter(t=>t.text.toLowerCase().includes(searchValue.toLowerCase()) )
   }
 
-
+  const saveTodos=(newTodos)=>{
+    setTasks(newTodos);
+    localStorage.setItem('TODOS_V1',JSON.stringify(tasks))
+  }
   const completeTodo=(text)=>{
     const todoIndex=tasks.findIndex(todo=>todo.text===text);
     const newTodos=[...tasks];
     newTodos[todoIndex].completed=true;
-    setTasks(newTodos);
+    saveTodos(newTodos);
   }
   const deleteTodo=(text)=>{
     const todoIndex=tasks.findIndex(todo=>todo.text===text);
     const newTodos=[...tasks];
     newTodos.splice(todoIndex,1)
-    setTasks(newTodos);
+    saveTodos(newTodos);
   }
   return (
    <AppUI
