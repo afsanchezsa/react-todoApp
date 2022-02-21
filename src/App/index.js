972743e463651882cddefba1c333940547fbd9f4
unknown <afsanchezsa@unal.edu.co>
2022-02-21 15:07:37 -0500

    custom hook

--- src/App/index.js
+++ src/App/index.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { AppUI } from './AppUI';
-
-/*const defaulTasks = [
+const      mytastk = [
   { 'id': 1, 'text': 'tarea 1', 'completed': true },
   { 'id': 2, 'text': 'tarea 2', 'completed': false },
   { 'id': 3, 'text': 'tarea 3', 'completed': false }
 
-]*/
+]
 
+function useLocalStorage(itemName,initialValue){
+  
+  const localStorageItem=localStorage.getItem(itemName);
+  let parsedItem;
+  if(!localStorageItem){
+    localStorage.setItem(itemName,JSON.stringify(initialValue));
+      parsedItem=initialValue
+  }else{
+      parsedItem=JSON.parse(localStorage.getItem(itemName));
+  }
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem=(newItem)=>{
+    setItem(newItem);
+    localStorage.setItem(itemName,JSON.stringify(newItem))
+  }
+
+  return [item,saveItem];
+}
 
 function App() {
-  const localStorageTodos=localStorage.getItem('TODOS_V1');
-  let defaulTasks;
-  if(!localStorageTodos){
-    localStorage.setItem('TODOS_V1',JSON.stringify([]));
-    defaulTasks=[]
-  }else{
-    defaulTasks=JSON.parse(localStorage.getItem('TODOS_V1'));
-  }
-  const [tasks, setTasks] = React.useState(defaulTasks);
+ 
+  const [tasks,saveTodos]=useLocalStorage('TODOS_V1',mytastk);
   const [searchValue, setSearchValue] = React.useState('');
   const completedTodos = tasks.filter(t => t.completed).length;
   const totalTodos = tasks.length;
@@ -29,10 +40,7 @@ function App() {
     searchedTodos=tasks.filter(t=>t.text.toLowerCase().includes(searchValue.toLowerCase()) )
   }
 
-  const saveTodos=(newTodos)=>{
-    setTasks(newTodos);
-    localStorage.setItem('TODOS_V1',JSON.stringify(tasks))
-  }
+ 
   const completeTodo=(text)=>{
     const todoIndex=tasks.findIndex(todo=>todo.text===text);
     const newTodos=[...tasks];
