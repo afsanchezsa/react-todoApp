c4cd603008b57a508c354ef1026951862e9c532f
unknown <afsanchezsa@unal.edu.co>
2022-02-21 09:51:49 -0500

    organizacion por carpetas

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import { TodoCounter } from './components/TodoCounter';
-import { TodoSearch } from './components/TodoSearch';
-import { TodoList } from './components/TodoList';
-import { TodoItem } from './components/TodoItem';
-import { CreateTodoButton } from './components/CreateTodoButton';
+import { TodoCounter } from '../components/TodoCounter';
+import { TodoSearch } from '../components/TodoSearch';
+import { TodoList } from '../components/TodoList';
+import { TodoItem } from '../components/TodoItem';
+import { CreateTodoButton } from '../components/CreateTodoButton';
 const defaulTasks = [
   { 'id': 1, 'text': 'tarea 1', 'completed': true },
   { 'id': 2, 'text': 'tarea 2', 'completed': false },
