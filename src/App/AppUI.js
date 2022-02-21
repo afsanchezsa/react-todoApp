72d1b956638bdad07175d94ec00ee79014ceb7c6
unknown <afsanchezsa@unal.edu.co>
2022-02-21 17:06:07 -0500

    mejor forma de consumir un contexto

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -6,6 +6,10 @@ import { TodoItem } from '../components/TodoItem';
 import { CreateTodoButton } from '../components/CreateTodoButton';
 import { TodoContex } from '../TodoContex'
 function AppUI() {
+
+  const value=React.useContext(TodoContex)
+  const {error,loading,searchedTodos,completeTodo,deleteTodo}=value
+
   return (
     <React.Fragment>
       <TodoCounter
@@ -15,21 +19,18 @@ function AppUI() {
       <TodoSearch
 
       />
-      <TodoContex.Consumer>
+     
 
-        
-        {(value)=>{
-          const {error,loading,searchedTodos,completeTodo,deleteTodo}=value
-          return(
+               
           <TodoList>
           {error && <p>Hubo un error....</p>}
           {loading && <p>Estamos cargando....</p>}
           {searchedTodos.map(t => (<TodoItem key={t.id} text={t.text} completed={t.completed} onComplete={() => completeTodo(t.text)}
             onDelete={() => deleteTodo(t.text)} />))}
 
-        </TodoList>)
-        }}
-      </TodoContex.Consumer>
+        </TodoList>
+       
+
       <CreateTodoButton />
 
     </React.Fragment>
